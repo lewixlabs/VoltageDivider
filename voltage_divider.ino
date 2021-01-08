ecc12b8f3da7181a05965e3b0491b93cf002002d
lewix <lewixlabs@gmail.com>
2021-01-08 15:28:31 +0100

    first commit

---
+++ voltage_divider.ino
@@ -0,0 +1,17 @@
+#include <SoftwareSerial.h>
+
+
+void setup()
+{
+  pinMode(A5, INPUT);
+  Serial.begin(115200);           //  setup serial
+}
+
+int analogResult = 0;
+void loop()
+{
+  analogResult = analogRead(A5);
+  Serial.println(analogResult);
+  Serial.println((float)(analogResult*5)/1023);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
